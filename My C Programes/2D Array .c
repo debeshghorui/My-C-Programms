b922730280f947853ae9a447542453e783689dfe
debeshghorui <165417580+debeshghorui@users.noreply.github.com>
2024-11-07 08:17:06 +0530

    Create 2D Array .c

---
+++ My C Programes/2D Array .c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int rows = 2, cols = 2;
+    int matrix1[2][2] = {{1, 2}, {3, 4}};
+    int matrix2[2][2] = {{5, 6}, {7, 8}};
+    int result[2][2];
+
+    // Adding corresponding elements of matrix1 and matrix2
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            result[i][j] = matrix1[i][j] + matrix2[i][j];
+        }
+    }
+
+    // Displaying the result matrix
+    printf("Resultant Matrix:\n");
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            printf("%d ", result[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
